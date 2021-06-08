75815d4af74a91d96129f9d7317737ef33f9a437
luffyZh <zhoudeyou@kuaishou.com>
2021-06-08 20:28:54 +0800

    feat: 优化拖拽点击

--- draggable/dragable.ts
+++ draggable/dragable.ts
@@ -15,7 +15,7 @@ const isAndroid = / Android /.test(navigator.userAgent);
  */
 function draggable($ele, adsorb = { x: 20, y: 80 }) {
   if (!$ele) {
-    throw new Error('必须是可拖拽元素');
+    throw new Error("必须是可拖拽元素");
   }
   // 开始时候的位置
   let startX = 0;
@@ -36,99 +36,98 @@ function draggable($ele, adsorb = { x: 20, y: 80 }) {
    * 开始拖拽的事件
    */
   $ele.addEventListener(
-      'touchstart',
-      function (event) {
-          startX = event.targetTouches[0].pageX;
-          startY = event.targetTouches[0].pageY;
+    "touchstart",
+    function (event) {
+      startX = event.targetTouches[0].pageX;
+      startY = event.targetTouches[0].pageY;
 
-          top = $ele.offsetTop;
-          left = $ele.offsetLeft;
+      top = $ele.offsetTop;
+      left = $ele.offsetLeft;
 
-          event.preventDefault();
-      },
-      false
+      event.preventDefault();
+    },
+    false
   );
 
   /**
    * 拖拽过程中的事件
    */
   $ele.addEventListener(
-      'touchmove',
-      function (event) {
-          // 偏移距离
-          const offsetX = event.targetTouches[0].pageX - startX;
-          const offsetY = event.targetTouches[0].pageY - startY;
+    "touchmove",
+    function (event) {
+      // 偏移距离
+      const offsetX = event.targetTouches[0].pageX - startX;
+      const offsetY = event.targetTouches[0].pageY - startY;
 
-          $ele.style.top = `${top + offsetY}px`;
-          $ele.style.left = `${left + offsetX}px`;
-          $ele.style.right = 'auto';
-          $ele.style.bottom = 'auto';
+      $ele.style.top = `${top + offsetY}px`;
+      $ele.style.left = `${left + offsetX}px`;
+      $ele.style.right = "auto";
+      $ele.style.bottom = "auto";
 
-          event.preventDefault();
-      },
-      false
+      event.preventDefault();
+    },
+    false
   );
 
   function touchDone(event) {
-      const dx = event.changedTouches[0].pageX - startX;
-      const dy = event.changedTouches[0].pageY - startY;
+    const dx = event.changedTouches[0].pageX - startX;
+    const dy = event.changedTouches[0].pageY - startY;
 
-      const ty = top + dy;
-      const tx = left + dx;
+    const ty = top + dy;
+    const tx = left + dx;
 
-      $ele.style.top = `${ty}px`;
-      $ele.style.left = `${tx}px`;
-      $ele.style.right = 'auto';
-      $ele.style.bottom = 'auto';
+    $ele.style.top = `${ty}px`;
+    $ele.style.left = `${tx}px`;
+    $ele.style.right = "auto";
+    $ele.style.bottom = "auto";
 
-      const adsorb_safe_x = cw - width - adsorb.x;
-      const adsorb_safe_y = ch - height - adsorb.y;
+    const adsorb_safe_x = cw - width - adsorb.x;
+    const adsorb_safe_y = ch - height - adsorb.y;
 
-      raf(() => {
-          let nx;
-          let ny = ty;
+    raf(() => {
+      let nx;
+      let ny = ty;
 
-          if (tx + width / 2 < cw / 2) {
-              nx = adsorb.x;
-          } else {
-              nx = adsorb_safe_x;
-          }
+      if (tx + width / 2 < cw / 2) {
+        nx = adsorb.x;
+      } else {
+        nx = adsorb_safe_x;
+      }
 
-          if (ty < adsorb.y) {
-              ny = adsorb.y;
-          } else if (ty > adsorb_safe_y) {
-              ny = adsorb_safe_y;
-          }
+      if (ty < adsorb.y) {
+        ny = adsorb.y;
+      } else if (ty > adsorb_safe_y) {
+        ny = adsorb_safe_y;
+      }
 
-          // 无需移动
-          if (Math.abs(tx - nx) < 5 && Math.abs(ty - ny) < 5) {
-              $ele.click();
-              if ($ele.firstElementChild) {
-                  $ele.firstElementChild.click();
-              }
-              return;
-          }
+      // 无需移动，并且认为进行了一次点击
+      if (Math.abs(tx - nx) < 5 && Math.abs(ty - ny) < 5) {
+        $ele.click();
+        return;
+      }
 
-          const MOVE_ANIM_INTER = isAndroid ? Math.abs(((250 * (tx - nx)) / 800) | 0) : 200;
+      const MOVE_ANIM_INTER = isAndroid
+        ? Math.abs(((250 * (tx - nx)) / 800) | 0)
+        : 200;
 
-          $ele.style.webkitTransition = `left ${MOVE_ANIM_INTER}ms ease-in-out, top ${MOVE_ANIM_INTER}ms ease-in-out`;
-          $ele.style.transition = `left ${MOVE_ANIM_INTER}ms ease-in-out, top ${MOVE_ANIM_INTER}ms ease-in-out`;
+      $ele.style.webkitTransition = `left ${MOVE_ANIM_INTER}ms ease-in-out, top ${MOVE_ANIM_INTER}ms ease-in-out`;
+      $ele.style.transition = `left ${MOVE_ANIM_INTER}ms ease-in-out, top ${MOVE_ANIM_INTER}ms ease-in-out`;
 
-          const onAnimationDone = () => {
-              $ele.style.webkitTransition = $ele.style.transition = 'none';
+      const onAnimationDone = () => {
+        $ele.style.webkitTransition = $ele.style.transition = "none";
 
-              $ele.removeEventListener('webkitTransitionEnd', onAnimationDone, false);
-              $ele.removeEventListener('transitionend', onAnimationDone, false);
-          };
+        $ele.removeEventListener("webkitTransitionEnd", onAnimationDone, false);
+        $ele.removeEventListener("transitionend", onAnimationDone, false);
+      };
 
-          $ele.addEventListener('webkitTransitionEnd', onAnimationDone, false);
-          $ele.addEventListener('transitionend', onAnimationDone, false);
+      $ele.addEventListener("webkitTransitionEnd", onAnimationDone, false);
+      $ele.addEventListener("transitionend", onAnimationDone, false);
 
-          $ele.style.top = `${ny}px`;
-          $ele.style.left = `${nx}px`;
-      });
+      $ele.style.top = `${ny}px`;
+      $ele.style.left = `${nx}px`;
+    });
   }
 
-  $ele.addEventListener('touchend', touchDone, true);
-  $ele.addEventListener('touchcancel', touchDone, true);
+  $ele.addEventListener("touchend", touchDone, true);
+  $ele.addEventListener("touchcancel", touchDone, true);
 }
